fecae26918405ded912fbc8d703db5d0c58cac59
rakkateichou <rakkateichou@gmail.com>
2024-08-06 18:57:16 +0800

    blog mobile

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,10 +1,10 @@
 export default function Blog() {
   return (
-    <main className="slide-in-left px-72 mt-10">
+    <main className="slide-in-left px-6 mt-10 sm:px-72 sm:mt-10">
       <div>
         <div>
-        <div className="flex justify-between text-xl">
-          <div>Exporting a Minecraft tree to Three.js</div>
+        <div className="flex justify-between text-nowrap text-xl">
+          <div className="truncate w-64 sm:w-full">Exporting a Minecraft tree to Three.js</div>
           <div className="opacity-75">03/08/24</div>
         </div>
         <div className="h-px mt-1 bg-sakura-1"/>
